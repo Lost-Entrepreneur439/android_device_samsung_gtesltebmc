d928514a6c9d32ee7773e0658b9ee3256d892561
Lost-Entrepreneur439 <78825510+Lost-Entrepreneur439@users.noreply.github.com>
2024-08-21 01:10:49 -0230

    bdroid_buildcfg.h: Modify for gtesltebmc

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy On5 Pro"
+#define BTM_DEF_LOCAL_NAME   "Samsung Galaxy Tab E 8.0"
 
 #define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
 #define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
